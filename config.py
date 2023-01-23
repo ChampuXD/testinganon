05176b80b87ed577fa205ba63588c793bd632917
Champu <112340896+shivanshhu@users.noreply.github.com>
2023-01-23 06:34:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -21,12 +21,12 @@ OWNER_ID = list(map(int, getenv("OWNER_ID", "1356469075").split()))
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
 
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/TheChampu/TheChampu")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/shivanshhu/testinganon")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv("GIT_TOKEN", None)
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FallenAssociation")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/DevilsHeavenMF")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/TheShivanshu")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/ll_Champu_ll")
 
 SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
 
